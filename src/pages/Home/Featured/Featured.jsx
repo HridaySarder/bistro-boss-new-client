9b5b77168bcddeb09d99c2a86de0124b6d67dec3
HridaySarder <hridoyaug12@gmail.com>
2023-12-24 23:48:09 +0600

    added featured item

---
+++ src/pages/Home/Featured/Featured.jsx
@@ -0,0 +1,26 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import featuredImg from "../../../assets/home/featured.jpg"
+import "./Featured.css"
+const Featured = () => {
+  return (
+    <div className="featured-item text-white pt-10 my-20">
+      <SectionTitle 
+      subHeading={"chect it out"}
+      heading={"Featured Item"}
+      ></SectionTitle>
+      <div className="md:flex justify-center items-center gap-5 pb-20 pt-12 px-36">
+      <div>
+        <img src={featuredImg} alt="" />
+      </div>
+      <div>
+        <p>March 20, 2023</p>
+        <p>WHERE CAN I GET SOME?</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti corrupti consectetur magni deserunt fugiat veritatis porro odit, pariatur sed aut, culpa perspiciatis nisi! Officia, repellat. Quam aliquam ullam itaque vero atque nihil veniam illum, harum temporibus. Quos facilis at, non inventore repellat commodi ipsa, molestias dolor, ducimus necessitatibus molestiae modi.</p>
+        <button className="btn btn-outline">Order Now</button>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
