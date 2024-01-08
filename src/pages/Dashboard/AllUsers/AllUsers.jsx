bde454cd436159eb817cba7f8fd9a11f0edaa70b
HridaySarder <hridoyaug12@gmail.com>
2024-01-08 22:37:14 +0600

    added admin route and other file

--- src/pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/pages/Dashboard/AllUsers/AllUsers.jsx
@@ -4,9 +4,8 @@ import { FaTrashAlt, FaUsers } from "react-icons/fa";
 import Swal from "sweetalert2";
 
 const AllUsers = () => {
-
   const axiosSecure = useAxiosSecure();
-  const { data: users = [],refetch } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
@@ -14,49 +13,46 @@ const AllUsers = () => {
     },
   });
 
-const handleDeleteUser = (user) => {
-  Swal.fire({
-    title: "Are you sure?",
-    text: "You won't be able to revert this!",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "Yes, delete it!",
-  }).then((result) => {
-    if (result.isConfirmed) {
-      axiosSecure.delete(`/users/${user._id}`).then((res) => {
-        if (res.data.deletedCount > 0) {
-          refetch();
-          Swal.fire({
-            title: "Deleted!",
-            text: "Your file has been deleted.",
-            icon: "success",
-          });
-        }
-      });
-    }
-  });
-}
+  const handleDeleteUser = (user) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/users/${user._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
 
-const handleMakeAdmin = user => {
-  axiosSecure.patch(`/users/admin/${user._id}`)
-  .then(res => {
-    console.log(res.data);
-    if(res.data.modifiedCount > 0){
-      refetch()
-      Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: `${user.name} is an Admin now.`,
-        showConfirmButton: false,
-        timer: 1500
-      });
-    }
-  })
-}
-
-  
+  const handleMakeAdmin = (user) => {
+    axiosSecure.patch(`/users/admin/${user._id}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${user.name} is an Admin now.`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -82,12 +78,19 @@ const handleMakeAdmin = user => {
                 <td>{user.name}</td>
                 <td>{user.email}</td>
                 <td>
-                { user.role === 'admin' ? 'Admin' : <button onClick={() =>  handleMakeAdmin(user)} className="btn bg-[#D1A054] btn-lg">
-                 <FaUsers className="text-white"></FaUsers>
-                 </button>}
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <button
+                      onClick={() => handleMakeAdmin(user)}
+                      className="btn bg-[#D1A054] btn-lg"
+                    >
+                      <FaUsers className="text-white"></FaUsers>
+                    </button>
+                  )}
                 </td>
                 <td>
-                <button
+                  <button
                     onClick={() => handleDeleteUser(user)}
                     className="btn bg-[#B91C1C] btn-lg"
                   >
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,55 +1,73 @@
 import { createContext, useEffect, useState } from "react";
-import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { app } from "../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 const auth = getAuth(app);
-export const AuthContext = createContext(null)
-const AuthProvider = ({children}) => {
-  const [user,setUser] = useState(null)
-  const [loading,setLoading] = useState(true)
+export const AuthContext = createContext(null);
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
-  const googleProvider = new GoogleAuthProvider()
+  const googleProvider = new GoogleAuthProvider();
 
-const createUser = (email,password) => {
-  setLoading(true)
-  return createUserWithEmailAndPassword(auth,email,password);
-}
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
-const googleSignIn = () => {
-  setLoading(true)
-  return signInWithPopup(auth,googleProvider);
-}
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
-const updateUserProfile = (name,photo) => {
- return updateProfile(auth.currentUser, {
-displayName:name,photoURL:photo
-  })
-} 
+  const updateUserProfile = (name, photo) => {
+    return updateProfile(auth.currentUser, {
+      displayName: name,
+      photoURL: photo,
+    });
+  };
 
-const signIn = (email,password) => {
-  setLoading(true)
-  return signInWithEmailAndPassword(auth,email,password)
-}
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
-const logOut = () => {
-  setLoading(true)
-  return signOut(auth)
-}
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
 
-useEffect(() => {
-  const unsubscribe = onAuthStateChanged(auth,currentUser => {
-    setUser(currentUser)
-    if(currentUser){
-      // get token store client
-    }
-    else{
-      // TODO:
-    }
-    setLoading(false)
-  })
-  return () => {
-    return unsubscribe();
-  }
-},[])
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        // get token store client
+        const userInfo = { email: currentUser.email };
+        axiosPublic.post("/jwt", userInfo).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        // TODO:
+        localStorage.removeItem("access-token");
+      }
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, [axiosPublic]);
 
   const authInfo = {
     user,
@@ -58,12 +76,10 @@ useEffect(() => {
     googleSignIn,
     updateUserProfile,
     signIn,
-    logOut
-  }
+    logOut,
+  };
   return (
-    <AuthContext.Provider value={authInfo}>
-      {children}
-    </AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
 };
 
