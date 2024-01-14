af586224ddd3e8465d1b53c437da727afe2eb636
HridaySarder <hridoyaug12@gmail.com>
2024-01-14 23:36:37 +0600

    added more files

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -6,17 +6,19 @@ const axiosSecure = axios.create({
   baseURL: "http://localhost:5000",
 });
 const useAxiosSecure = () => {
-  const { logOut } = useAuth();
   const navigate = useNavigate();
+  const { logOut } = useAuth();
 
+  // request interceptor to add authorization header for every secure call to teh api
   axiosSecure.interceptors.request.use(
     function (config) {
       const token = localStorage.getItem("access-token");
-      // console.log("request stopped by interceptors", token);
+      // console.log('request stopped by interceptors', token)
       config.headers.authorization = `Bearer ${token}`;
       return config;
     },
     function (error) {
+      // Do something with request error
       return Promise.reject(error);
     }
   );
@@ -28,7 +30,8 @@ const useAxiosSecure = () => {
     },
     async (error) => {
       const status = error.response.status;
-      // console.log("status error in the interceptos", status);
+      // console.log('status error in the interceptor', status);
+      // for 401 or 403 logout the user and move the user to the login
       if (status === 401 || status === 403) {
         await logOut();
         navigate("/login");
--- src/pages/Dashboard/UpdateItem/UpdateItem.jsx
+++ src/pages/Dashboard/UpdateItem/UpdateItem.jsx
@@ -1,43 +1,47 @@
 import { useLoaderData } from "react-router-dom";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+import { useForm } from "react-hook-form";
 import Swal from "sweetalert2";
 import useAxiosPublic from "../../../hooks/useAxiosPublic";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
-import { useForm } from "react-hook-form";
 
 const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
 const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const UpdateItem = () => {
-  const {name,category,recipe,price,_id} = useLoaderData()
-  const axiosPubic = useAxiosPublic();
+  const { name, category, recipe, price, _id } = useLoaderData();
+
+  const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic();
   const axiosSecure = useAxiosSecure();
-  const { register, handleSubmit, reset } = useForm();
   const onSubmit = async (data) => {
     console.log(data);
+    // image upload to imgbb and then get an url
     const imageFile = { image: data.image[0] };
-    const res = await axiosPubic.post(image_hosting_api, imageFile, {
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
       headers: {
-        "Content-Type": "multipart/form-data",
+        "content-type": "multipart/form-data",
       },
     });
     if (res.data.success) {
+      // now send the menu item data to the server with the image url
       const menuItem = {
         name: data.name,
-        image: res.data.data.display_url,
         category: data.category,
-        recipe: data.recipe,
         price: parseFloat(data.price),
+        recipe: data.recipe,
+        image: res.data.data.display_url,
       };
+      //
       const menuRes = await axiosSecure.patch(`/menu/${_id}`, menuItem);
       console.log(menuRes.data);
-      if (menuRes.data.insertedId) {
-        reset();
-        // success message
+      if (menuRes.data.modifiedCount > 0) {
+        // show success popup
+        // reset();
         Swal.fire({
           position: "top-end",
           icon: "success",
-          title: `${data.name} is updare to the menu`,
+          title: `${data.name} is updated to the menu.`,
           showConfirmButton: false,
           timer: 1500,
         });
@@ -45,9 +49,13 @@ const UpdateItem = () => {
     }
     console.log("with image url", res.data);
   };
+
   return (
     <div>
-      <SectionTitle heading={'Update Item'}></SectionTitle>
+      <SectionTitle
+        heading="Update an Item"
+        subHeading="Refresh info"
+      ></SectionTitle>
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="form-control w-full my-6">
@@ -59,31 +67,33 @@ const UpdateItem = () => {
               defaultValue={name}
               placeholder="Recipe Name"
               {...register("name", { required: true })}
+              required
               className="input input-bordered w-full"
             />
           </div>
           <div className="flex gap-6">
-            {/* category name */}
+            {/* category */}
             <div className="form-control w-full my-6">
               <label className="label">
-                <span className="label-text">Category Name*</span>
+                <span className="label-text">Category*</span>
               </label>
               <select
                 defaultValue={category}
                 {...register("category", { required: true })}
                 className="select select-bordered w-full"
               >
-                <option disabled value={"default"}>
-                  Category
+                <option disabled value="default">
+                  Select a category
                 </option>
-                <option value={"salad"}>Salad</option>
-                <option value={"pizza"}>Pizza</option>
-                <option value={"soup"}>Soup</option>
-                <option value={"dessert"}>Dessert</option>
-                <option value={"drinks"}>Drinks</option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
               </select>
             </div>
-            {/* Pirce */}
+
+            {/* price */}
             <div className="form-control w-full my-6">
               <label className="label">
                 <span className="label-text">Price*</span>
@@ -97,17 +107,19 @@ const UpdateItem = () => {
               />
             </div>
           </div>
-          <label className="form-control">
-            <div className="label">
-              <span className="label-text">Recipe Details*</span>
-            </div>
+          {/* recipe details */}
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Recipe Details</span>
+            </label>
             <textarea
-            defaultValue={recipe}
+              defaultValue={recipe}
               {...register("recipe")}
               className="textarea textarea-bordered h-24"
               placeholder="Recipe Details"
             ></textarea>
-          </label>
+          </div>
+
           <div className="form-control w-full my-6">
             <input
               {...register("image", { required: true })}
@@ -115,10 +127,8 @@ const UpdateItem = () => {
               className="file-input w-full max-w-xs"
             />
           </div>
-          <button className="btn bg-[#B58130]">
-            Update Menu Item
-            
-          </button>
+
+          <button className="btn">Update menu Item</button>
         </form>
       </div>
     </div>
